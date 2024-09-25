5a3c6d67939c9e382c63e27360f94aef0ab404e2
Waad Albadi <waad@globcom.com>
2024-09-25 16:06:16 +0400

    first commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+[ApiController]
+[Route("api/[controller]")]
+public class UserController(DataContext context) : ControllerBase
+{
+
+    [HttpGet]
+public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+{
+    var users = await context.Users.ToListAsync();
+    return users;
+}
+
+
+ [HttpGet("{id:int}")] //api/user/2
+public async Task<ActionResult<AppUser>> GetUser(int id)
+{
+    var user =await context.Users.FindAsync(id);
+    if (user == null) return NotFound();
+    return user;
+}
+}
